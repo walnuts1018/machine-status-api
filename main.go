0bb20159dfbfaaa2de2bbd0b5a3c00c902a7ae32
walnuts1018 <r.juglans.1018@gmail.com>
2022-10-28 14:21:50 +0900

    /boot/aliceを追加

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stianeikeland/go-rpio"
 )
 
 func main() {
@@ -14,17 +17,34 @@ func main() {
 			"message": "Hello World!",
 		})
 	})
-	r.PUT("/somePut", boot_machine)
+	r.PUT("/boot/alice", boot_alice)
 	log.Fatal(r.Run())
 }
 
-func boot_machine(c *gin.Context, name string) {
-	
-    if err != nil{
-        c.String(http.StatusInternalServerError, "Server Error")
-        return
-    }
-    c.JSON(http.StatusCreated, gin.H{
-        "status": "ok",
-    })
+func boot_alice(c *gin.Context) {
+
+	var push_time time.Duration = 800 //ミリ秒
+
+	// gpio処理開始
+	err := rpio.Open()
+
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Server Error")
+		return
+	}
+
+	pin_boot := rpio.Pin(40) // GPIO40*ピン*<-ピンであることに注意
+	pin_boot.Output()
+
+	// push_timeミリ秒出力（aliceの電源スイッチピンをショート）
+	pin_boot.High()
+	time.Sleep(push_time * time.Millisecond)
+	pin_boot.Low()
+
+	//gpi処理終わり
+	rpio.Close()
+
+	c.JSON(http.StatusCreated, gin.H{
+		"status": "ok",
+	})
 }
